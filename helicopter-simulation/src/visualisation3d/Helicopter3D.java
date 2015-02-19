78179db4854ec9ca246f2015379a10864b91cfaf
mphyatt <Marcus.Hyatt@gmx.de>
2015-02-19 04:42:29 +0000

    Implemented 3DModel representation of helicopter
    Instead of having a sphere representing the helicopter, we now have a 3D
    Model of a helicopter. Slight changes have to be made to make the
    simulation look better.

--- helicopter-simulation/src/visualisation3d/Helicopter3D.java
+++ helicopter-simulation/src/visualisation3d/Helicopter3D.java
@@ -9,6 +9,7 @@ import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Sphere;
 
 import mathematicalModel.CrashTrajectory;
@@ -20,7 +21,7 @@ public class Helicopter3D {
 
 	public Position pos;
 
-	private Geometry geoObject;
+	private Spatial helicopter3DModel;
 
 	/**
 	 * This function is responsible for creating the top level 3D object that
@@ -29,13 +30,18 @@ public class Helicopter3D {
 	 * @param assetManager
 	 */
 	private void drawHelicopter(AssetManager assetManager) {
-		Sphere cube1Mesh = new Sphere(10, 10, 10);// size of object
-		this.geoObject = new Geometry("My Textured Box", cube1Mesh);
-		Material cube1Mat = new Material(assetManager,
+		//loading the helicopter model
+		helicopter3DModel=assetManager.loadModel("Models/helicoptero_1.obj");
+		//making it bigger
+		helicopter3DModel.scale(5);
+		//rotating it so that it looks into the right direction
+		helicopter3DModel.rotate(0, (float) Math.PI, 0);
+
+		Material helicopterMaterial = new Material(assetManager,
 				"Common/MatDefs/Misc/Unshaded.j3md");
 
-		cube1Mat.setColor("Color", ColorRGBA.White);
-		this.geoObject.setMaterial(cube1Mat);
+		helicopterMaterial.setColor("Color", ColorRGBA.White);
+		this.helicopter3DModel.setMaterial(helicopterMaterial);
 	}
 
 	/**
@@ -58,7 +64,7 @@ public class Helicopter3D {
 	}
 
 	public static ArrayList<Position> positions(){
-		Trajectory traject = new Trajectory(new Helicopter(null, 200, 0, 150, 0, new Position(0,200,0), 45));
+		Trajectory traject = new Trajectory(new Helicopter(null, 200, 0, 50, 0, new Position(0,200,0), 45));
 		CrashTrajectory.calculateThrowTrajectory(traject);
 		return traject.getTrajectory();
 	}
@@ -71,7 +77,7 @@ public class Helicopter3D {
 	 * @param rootNode
 	 */
 	public void addToRootNode(Node rootNode) {
-		rootNode.attachChild(this.geoObject);
+		rootNode.attachChild(this.helicopter3DModel);
 	}
 
 	/**
@@ -82,7 +88,7 @@ public class Helicopter3D {
 	 * @param z
 	 */
 	public void updatePosition() {
-		this.geoObject.setLocalTranslation(this.pos.toVector3f()); //position of object = top to bottom, zoom in / out, left to right 
+		this.helicopter3DModel.setLocalTranslation(this.pos.toVector3f()); //position of object = top to bottom, zoom in / out, left to right 
 	}
 
 }
