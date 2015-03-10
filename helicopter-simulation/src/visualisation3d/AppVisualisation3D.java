8bc95b0a094d26e87cc5402eb2535594d32d3395
Roberto Catra <2036794c@sibu.(none)>
2015-03-10 00:14:09 +0000

    Updated visulisation of stats

--- helicopter-simulation/src/visualisation3d/AppVisualisation3D.java
+++ helicopter-simulation/src/visualisation3d/AppVisualisation3D.java
@@ -65,7 +65,12 @@ public class AppVisualisation3D extends SimpleApplication {
 		flyCam.setEnabled(true);
 		flyCam.setMoveSpeed(300);
 		flyCam.setDragToRotate(true);
-				
+		
+		
+		setDisplayFps(false);
+		setDisplayStatView(false);
+		
+		
 		initKeys();
 	}
 
